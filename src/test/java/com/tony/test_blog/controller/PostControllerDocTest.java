c2ba57767759cb46d3c0b9b0ba0f359e1f7cbe0a
dohyun_lim <dohyun_lim@tmax.co.kr>
2023-04-19 13:23:30 +0900

    RestDoc init
    Motivation:
    
    let client know what and how to use api
    
    Modification:
    
    used Spring Rest Docs to generate rest api page
    
    Result:
    
    can view api page at localhost:8080/docs/index.html

---
+++ src/test/java/com/tony/test_blog/controller/PostControllerDocTest.java
@@ -0,0 +1,55 @@
+package com.tony.test_blog.controller;
+
+import com.tony.test_blog.domain.Post;
+import com.tony.test_blog.repository.PostRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.restdocs.RestDocumentationContextProvider;
+import org.springframework.restdocs.RestDocumentationExtension;
+import org.springframework.restdocs.mockmvc.MockMvcRestDocumentation;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
+
+@SpringBootTest
+@ExtendWith(RestDocumentationExtension.class)
+public class PostControllerDocTest {
+
+    private MockMvc mockMvc;
+
+    @Autowired
+    private PostRepository postRepository;
+
+    @BeforeEach
+    void setUp(WebApplicationContext webApplicationContext, RestDocumentationContextProvider restDocumentation) {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext)
+                .apply(documentationConfiguration(restDocumentation))
+                .build();
+    }
+
+    @Test
+    @DisplayName("글 단건 조회 테스트")
+    void test1() throws Exception {
+        // given
+        Post post = Post.builder()
+                .title("123456789012345")
+                .content("bar")
+                .build();
+        postRepository.save(post);
+        // expected
+        mockMvc.perform(MockMvcRequestBuilders.get("/posts/{postId}",1).accept(MediaType.APPLICATION_JSON))
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andDo(MockMvcRestDocumentation.document("index"));
+    }
+}
